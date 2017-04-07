13ea8f55719d45845bacd0301c733d46d0ce67b9
romina <romysory@gmail.com>
2017-04-06 22:28:08 -0300

    Se agrega clase calificacion

---
+++ src/main/java/domain/Calificacion.java
@@ -0,0 +1,53 @@
+package domain;
+
+import java.util.Date;
+
+public class Calificacion {
+
+    private Integer id;
+    private String value;
+    private Date createdAt;
+    private Date updatedAt;
+
+    
+
+    public Calificacion(Integer id, String value, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.value = value;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
